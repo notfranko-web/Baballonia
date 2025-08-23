3b1ef1e393120364e49571187d4dc2c57070d96a
RedInJector <imaxprots@gmail.com>
2025-08-24 01:51:39 +0200

    fix: eye processing function

--- src/Baballonia/Services/Inference/EyeProcessingPipeline.cs
+++ src/Baballonia/Services/Inference/EyeProcessingPipeline.cs
@@ -41,11 +41,50 @@ public class EyeProcessingPipeline : DefaultProcessingPipeline
         if(Filter != null)
             inferenceResult = Filter.Filter(inferenceResult);
 
+        ProcessExpressions(ref inferenceResult);
+
         InvokeFilteredResultEvent(inferenceResult);
 
         frame.Dispose();
         transformed.Dispose();
 
         return inferenceResult;
+    }
+
+    private bool ProcessExpressions(ref float[] arKitExpressions)
+    {
+        if (arKitExpressions.Length < Utils.EyeRawExpressions)
+            return false;
+
+        const float mulV = 2.0f;
+        const float mulY = 2.0f;
+
+        var leftPitch = arKitExpressions[0] * mulY - mulY / 2;
+        var leftYaw = arKitExpressions[1] * mulV - mulV / 2;
+        var leftLid = 1 - arKitExpressions[2];
+
+        var rightPitch = arKitExpressions[3] * mulY - mulY / 2;
+        var rightYaw = arKitExpressions[4] * mulV - mulV / 2;
+        var rightLid = 1 - arKitExpressions[5];
+
+        var eyeY = (leftPitch * leftLid + rightPitch * rightLid) / (leftLid + rightLid);
+
+        var leftEyeYawCorrected = rightYaw * (1 - leftLid) + leftYaw * leftLid;
+        var rightEyeYawCorrected = leftYaw * (1 - rightLid) + rightYaw * rightLid;
+
+        // [left pitch, left yaw, left lid...
+        float[] convertedExpressions = new float[Utils.EyeRawExpressions];
+
+        // swap eyes at this point
+        convertedExpressions[0] = rightEyeYawCorrected; // left pitch
+        convertedExpressions[1] = eyeY;                   // left yaw
+        convertedExpressions[2] = rightLid;               // left lid
+        convertedExpressions[3] = leftEyeYawCorrected;  // right pitch
+        convertedExpressions[4] = eyeY;                   // right yaw
+        convertedExpressions[5] = leftLid;                // right lid
+
+        arKitExpressions = convertedExpressions;
+
+        return true;
     }
 }
